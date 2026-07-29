36a28b5ff76b3a4d5bb8f48dc013ea197c1d7d60
Muslima Zokirjonova <muslimazokirjonova@webster.edu>
2026-07-29 11:24:25 +0500

    settings.py

--- root/settings.py
+++ root/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = "/var/www/task_flow/static"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
